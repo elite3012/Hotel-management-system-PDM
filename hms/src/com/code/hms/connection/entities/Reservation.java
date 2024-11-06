983d3ad84f7e5b8110a5caee5d190a81addb7979
HelicopterNguyen <itcsiu23030@student.hcmiu.edu.vn>
2024-11-06 22:38:51 +0700

    feat: add getters and setters for the attributes Reservation_id, Customer_id, Room_id, Service_id, CheckinDate, CheckoutDate, TotalDays, RoomNumber

--- hms/src/com/code/hms/connection/entities/Reservation.java
+++ hms/src/com/code/hms/connection/entities/Reservation.java
@@ -41,4 +41,69 @@ public class Reservation implements Serializable {
     private String RoomNumber;
 
     public Reservation(){}
+
+    public int getReservationID() {
+        return Reservation_id;
+    }
+
+    public void setReservationID(int Reservation_id) {
+        this.Reservation_id = Reservation_id;
+    }
+
+    public int getCustomerID() {
+        return Customer_id;
+    }
+
+    public void setCustomerID(int Customer_id) {
+        this.Customer_id = Customer_id;
+    }
+
+    public int getRoomID() {
+        return Room_id;
+    }
+
+    public void setRoomId(int Room_id) {
+        this.Room_id = Room_id;
+    }
+
+    public int getServiceId() {
+        return Service_id;
+    }
+
+    public void setServiceId(int Service_id) {
+        this.Service_id = Service_id;
+    }
+
+    public Date getCheckinDate() {
+        return CheckinDate;
+    }
+
+    public void setCheckinDate(Date CheckinDate) {
+        this.CheckinDate = CheckinDate;
+    }
+
+    public Date getCheckoutDate() {
+        return CheckoutDate;
+    }
+
+    public void setCheckoutDate(Date CheckoutDate) {
+        this.CheckoutDate = CheckoutDate;
+    }
+
+    public int getTotalDays() {
+        return TotalDays;
+    }
+
+    public void setTotalDays(int TotalDays) {
+        this.TotalDays = TotalDays;
+    }
+
+    public String getRoomNumber() {
+        return RoomNumber;
+    }
+
+    public void setRoomNumber(String RoomNumber) {
+        this.RoomNumber = RoomNumber;
+    }
+
 }
