e83f4309c43b1bf0d680b56d1a0deedd442a9171
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2024-11-13 23:36:54 +0700

    feat: add our hotel tab

--- hms/src/main/java/com/code/hms/ui/CustomerUI.java
+++ hms/src/main/java/com/code/hms/ui/CustomerUI.java
@@ -1,11 +1,13 @@
 package com.code.hms.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CustomerUI {
     public static class HotelInfoGUI {
         static JFrame frame;
         static JPanel panel;
+        static JLabel HotelInfoTab;
 
         public HotelInfoGUI() {
             createMainGUI();
@@ -23,6 +25,12 @@ public class CustomerUI {
             panel = new JPanel();
             panel.setLayout(null);
             frame.getContentPane().add(panel);
+
+            // HotelInfoTab setup
+            HotelInfoTab = new JLabel("OUR HOTEL");
+            HotelInfoTab.setFont(new Font("Mulish", Font.BOLD, 25));
+            HotelInfoTab.setBounds(35, 145, 250, 60);
+            panel.add(HotelInfoTab);
         }
-        }
+    }
 }
