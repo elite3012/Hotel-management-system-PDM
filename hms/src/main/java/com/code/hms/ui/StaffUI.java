d435098a9ba91a70b8c6d51dba1cee8cfb7df302
tqkhanh765 <tqkhanh.work@gmail.com>
2024-11-13 21:45:34 +0700

    feat: initialize staff ui

---
+++ hms/src/main/java/com/code/hms/ui/StaffUI.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+
+public class StaffUI {
+    static JLabel background;
+    static JFrame frame;
+    static JPanel panel;
+    static JButton main_menu;
+
+
+    public StaffUI() {
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose: Receptionist(1)/Housekeeper(2)/Admin(3)\n");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                createReceptionistUI();
+                break;
+            case 2:
+                createHousekeeperUI();
+                break;
+            case 3:
+                createAdminUI();
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                System.exit(0);
+        }*/
+        createReceptionistUI();
+    }
+
+    private void createReceptionistUI () {
+        //frame
+        frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(1440, 900);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+
+        //panel
+        panel = new JPanel();
+        panel.setLayout(null);
+        frame.getContentPane().add(panel);
+    }
+}
