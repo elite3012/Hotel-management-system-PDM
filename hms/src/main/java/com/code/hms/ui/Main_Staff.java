82ede7ce397c002edc60d1c93fd9b20c9693b90e
tqkhanh765 <tqkhanh.work@gmail.com>
2024-11-13 22:01:58 +0700

    fix: rename Main_Staff class

--- hms/src/main/java/com/code/hms/ui/Main_Staff.java
+++ hms/src/main/java/com/code/hms/ui/Main_Staff.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 
-public class Main {
+public class Main_Staff {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
