c48297589b50abc9ffb69e7175dc40bb178e8a98
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2024-11-13 23:34:42 +0700

    feat: add frame and panel

--- hms/src/main/java/com/code/hms/ui/CustomerUI.java
+++ hms/src/main/java/com/code/hms/ui/CustomerUI.java
@@ -1,4 +1,28 @@
 package com.code.hms.ui;
 
+import javax.swing.*;
+
 public class CustomerUI {
+    public static class HotelInfoGUI {
+        static JFrame frame;
+        static JPanel panel;
+
+        public HotelInfoGUI() {
+            createMainGUI();
+        }
+
+        private void createMainGUI() {
+            // Frame setup
+            frame = new JFrame();
+            frame.setSize(1440, 900);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+            frame.setResizable(true);
+
+            // Panel setup
+            panel = new JPanel();
+            panel.setLayout(null);
+            frame.getContentPane().add(panel);
+        }
+        }
 }
