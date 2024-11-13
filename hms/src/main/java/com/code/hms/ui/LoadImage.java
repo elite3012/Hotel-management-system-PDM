fee1ed9195ada7e26b36d1dbd5ec611a39a53761
tqkhanh765 <tqkhanh.work@gmail.com>
2024-11-13 21:57:30 +0700

    feat: add LoadImage class
    LoadImage.java is responsible for loading and scaling images

---
+++ hms/src/main/java/com/code/hms/ui/LoadImage.java
@@ -0,0 +1,28 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class LoadImage{
+    public static ImageIcon loadImage(String imagePath) {
+        try {
+            BufferedImage image = ImageIO.read(new File(imagePath));
+            return new ImageIcon(image);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static ImageIcon loadScaledImage(String path, int width, int height) {
+        try {
+            Image img = ImageIO.read(new File(path));
+            Image scaledImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            return new ImageIcon(scaledImg);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
