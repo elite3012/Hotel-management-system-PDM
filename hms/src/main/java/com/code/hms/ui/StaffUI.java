d413b5705d6b58ecea6d07a8a9e6ca4ad7525cc1
tqkhanh765 <tqkhanh.work@gmail.com>
2024-11-14 01:02:32 +0700

    fix: rescale components

--- hms/src/main/java/com/code/hms/ui/StaffUI.java
+++ hms/src/main/java/com/code/hms/ui/StaffUI.java
@@ -12,6 +12,7 @@ public class StaffUI {
     static JButton main_menu;
     static JLabel available_unavailable;
     static JLabel RoomManagementTab;
+    static JLabel ReservationTab;
 
 
     public StaffUI() {
@@ -40,11 +41,11 @@ public class StaffUI {
     private void initializeUI(){
         //frame
         frame = new JFrame();
-        frame.setVisible(true);
-        frame.setSize(1440, 900);
+        frame.setBounds(0, 0, 1280, 672);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setResizable(true);
+        frame.setVisible(true);
 
         //panel
         panel = new JPanel();
@@ -54,26 +55,30 @@ public class StaffUI {
     private void createReceptionistUI () {
         // RoomManagementTab setup
         RoomManagementTab = new JLabel("ROOMS");
-        RoomManagementTab.setFont(new Font("Mulish", Font.BOLD, 25));
-        RoomManagementTab.setBounds(35, 145, 250, 60);
+        RoomManagementTab.setFont(new Font("Mulish", Font.BOLD, 23));
+        RoomManagementTab.setBounds(32, 115, 235, 33);
+        RoomManagementTab.setVisible(true);
         panel.add(RoomManagementTab);
 
         //main menu button
-        main_menu = new JButton(LoadImage.loadScaledImage("hms/src/main/java/com/code/hms/assets/Main_menu_Button.png",197,38));
-        main_menu.setBounds(21, 23, 197, 38);
+        main_menu = new JButton(LoadImage.loadScaledImage("hms/src/main/java/com/code/hms/assets/Main_menu_Button.png",175,28));
+        main_menu.setBounds(19,17,175,28);
         main_menu.setBorderPainted(false);       // Remove border
         main_menu.setBackground(null);
         main_menu.setHorizontalAlignment(SwingConstants.CENTER);
         main_menu.setVerticalAlignment(SwingConstants.CENTER);
+        main_menu.setVisible(true);
         panel.add(main_menu);
 
         //available_unavailable labels
-        available_unavailable = new JLabel(LoadImage.loadScaledImage("hms/src/main/java/com/code/hms/assets/Rooms_Available_Unavailable.png", 447, 47));
-        available_unavailable.setBounds(647,800,447,47);
+        available_unavailable = new JLabel(LoadImage.loadScaledImage("hms/src/main/java/com/code/hms/assets/Rooms_Available_Unavailable.png", 298,28));
+        available_unavailable.setBounds(576, 576, 399, 35);
         available_unavailable.setVisible(true);
         panel.add(available_unavailable);
-        background = new JLabel(LoadImage.loadScaledImage("hms/src/main/java/com/code/hms/assets/Rooms_BG.png",1440,900));
-        background.setBounds(0, 0, 1440, 900);
+
+        //background
+        background = new JLabel(LoadImage.loadScaledImage("hms/src/main/java/com/code/hms/assets/Rooms_BG.png",1280,672));
+        background.setBounds(0, 0, 1280,672);
         background.setVisible(true);
         panel.add(background);
     }
