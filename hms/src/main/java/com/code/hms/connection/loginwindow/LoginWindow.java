3acb6355d04824ebaaad24caa16b2cc02de68c26
hoangnv63 <vh2005dn7@gmail.com>
2024-11-14 11:07:53 +0700

    feat: add LoginWindow constructor

--- hms/src/main/java/com/code/hms/connection/loginwindow/LoginWindow.java
+++ hms/src/main/java/com/code/hms/connection/loginwindow/LoginWindow.java
@@ -8,4 +8,13 @@ public class LoginWindow extends JFrame {
     static JLabel usernameLbl, passwordLbl;
     static JButton loginButton;
     static JLabel forgotPasswordButton, signUpButton;
+
+    public LoginWindow() {
+
+        //set up frame
+        setTitle("Login");
+        setSize(1280, 672);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 }
