a8f5df37f578dd08d550a16fd790597e19a58f9b
hoangnv63 <vh2005dn7@gmail.com>
2024-11-14 12:25:03 +0700

    feat: create panel and add components

--- hms/src/main/java/com/code/hms/connection/loginwindow/LoginWindow.java
+++ hms/src/main/java/com/code/hms/connection/loginwindow/LoginWindow.java
@@ -16,5 +16,23 @@ public class LoginWindow extends JFrame {
         setSize(1280, 672);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        // Create panel
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        // Add components
+        usernameLbl = new JLabel("Username:");
+        usernameLbl.setBounds(100, 50, 300, 50);
+        usernameField = new JTextField();
+        usernameField.setBounds(100, 100, 300, 50);
+
+        passwordLbl = new JLabel("Password:");
+        passwordLbl.setBounds(100, 150, 300, 50);
+        passwordField = new JPasswordField();
+        passwordField.setBounds(100, 200, 300, 50);
+
+        loginButton = new JButton("Login");
+        loginButton.setBounds(100, 300, 300, 50);
     }
 }
