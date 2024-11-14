3b5eb1d22e2f644e23714bbd413b77f0ed7a7782
hoangnv63 <vh2005dn7@gmail.com>
2024-11-14 11:04:42 +0700

    add: create LoginWindow class

---
+++ hms/src/main/java/com/code/hms/connection/loginwindow/LoginWindow.java
@@ -0,0 +1,11 @@
+package com.code.hms.connection.loginwindow;
+
+import javax.swing.*;
+
+public class LoginWindow extends JFrame {
+    static JTextField usernameField;
+    static JPasswordField passwordField;
+    static JLabel usernameLbl, passwordLbl;
+    static JButton loginButton;
+    static JLabel forgotPasswordButton, signUpButton;
+}
