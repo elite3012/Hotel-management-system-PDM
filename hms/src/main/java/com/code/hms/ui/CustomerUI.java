8f7b07c082e9c9e98272480f43de38053afa4171
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2024-11-17 23:36:33 +0700

    fix: delete unnecessary package

--- hms/src/main/java/com/code/hms/ui/CustomerUI.java
+++ hms/src/main/java/com/code/hms/ui/CustomerUI.java
@@ -2,8 +2,6 @@ package com.code.hms.ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class CustomerUI {
     static JFrame frame;
