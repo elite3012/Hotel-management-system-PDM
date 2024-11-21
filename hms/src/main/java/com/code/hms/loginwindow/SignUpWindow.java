b94a029a22906b70123a1642d8f023264dd7a5c0
Van Anh <anhoconnell@gmail.com>
2024-11-22 06:10:20 +0700

    feat: declare variables

--- hms/src/main/java/com/code/hms/loginwindow/SignUpWindow.java
+++ hms/src/main/java/com/code/hms/loginwindow/SignUpWindow.java
@@ -1,4 +1,39 @@
 package com.code.hms.loginwindow;
 
-public class SignUpWindow {
+import javax.swing.*;
+import java.awt.*;
+
+
+public class SignUpWindow extends JFrame {
+
+    static JTextField firstNameField;
+    static JTextField lastNameField;
+    static JTextField dateOfBirthField;
+    static JTextField idNumberField;
+    static JTextField nationalityField;
+    static JTextField addressField;
+    static JTextField phoneNumberField;
+    static JTextField emailField;
+    static JTextField usernameField;
+    static JTextField roleField;
+
+    static JPasswordField passwordField;
+    static JPasswordField confirmPasswordField;
+
+    static JLabel firstNameLabel;
+    static JLabel lastNameLabel;
+    static JLabel dateOfBirthLabel;
+    static JLabel idNumberLabel;
+    static JLabel nationalityLabel;
+    static JLabel addressLabel;
+    static JLabel phoneNumberLabel;
+    static JLabel emailLabel;
+    static JLabel usernameLabel;
+    static JLabel passwordLabel;
+    static JLabel confirmPasswordLabel;
+    static JLabel roleLabel;
+
+    static JButton signInButton;
+
+
 }
