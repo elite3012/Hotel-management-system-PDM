f4adf01c104a7ac51b7caa77d251c8cb7f4a696d
Quys Tran <quyphuctran1@gmail.com>
2024-11-22 16:22:52 +0700

    fix: enhance and improve code (3)

--- hms/src/main/java/com/code/hms/loginwindow/LoadImage.java
+++ hms/src/main/java/com/code/hms/loginwindow/LoadImage.java
@@ -2,6 +2,7 @@ package com.code.hms.loginwindow;
 
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -14,19 +15,25 @@ public class LoadImage {
 
     public LoadImage(String path, int width, int height) {
         try {
-            // Load and resize the image to fit the specified width and height
-            image = ImageIO.read(new File(path));
+            File file = new File(path);
+            if (!file.exists()) {
+                throw new IllegalArgumentException("File not found: " + path);
+            }
+
+            image = ImageIO.read(file);
             Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+
             image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
             Graphics2D g2d = image.createGraphics();
+            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
             g2d.drawImage(scaledImage, 0, 0, null);
             g2d.dispose();
         } catch (IOException e) {
+            System.err.println("Error loading image from path: " + path);
             e.printStackTrace();
         }
     }
 
-    // Method to get the image as an icon
     public ImageIcon getImageIcon() {
         return new ImageIcon(image);
     }
