77f276638f1825c85f3984b34c4cfc8f5f51f153
Quys Tran <quyphuctran1@gmail.com>
2024-11-24 21:13:32 +0700

    feat: add DAO classes

---
+++ hms/src/main/java/com/code/hms/dao/BillingDAO.java
@@ -0,0 +1,23 @@
+package com.code.hms.dao;
+
+import com.code.hms.entities.Billing;
+
+public interface BillingDAO {
+    //Save to database
+    public void saveBilling(Billing billing);
+
+    //Delete the bill
+    public void deleteBilling(int billingId);
+
+    // Find bill by id
+    public Billing getBillingByID(int billingId);
+
+    // Find bill by reservation id
+    public Billing getBillingByReservationID(int reservationId);
+
+    // Find bill by room number
+    public Billing getBillingByRoomNumber(int roomId);
+
+    // Find the last bill
+    public Billing getLastBilling();
+}
---
+++ hms/src/main/java/com/code/hms/entities/Review.java
@@ -0,0 +1,111 @@
+package com.code.hms.entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "User_ID", nullable = false) // Specifies the foreign key column
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "Reservation_ID", nullable = false) // Specifies the foreign key column
+    private Reservation reservation;
+
+    @Column(name = "Rating")
+    private int rating;
+
+    @Column(name = "Comment")
+    private String comment;
+
+    @Column(name = "ReviewDate")
+    private Date reviewDate;
+
+    // No-argument constructor (required by JPA)
+    public Review() {
+    }
+
+    // Constructor
+    public Review(User user, Reservation reservation, int rating, String comment, Date reviewDate) {
+        this.user = user;
+        this.reservation = reservation;
+        this.rating = rating;
+        this.comment = comment;
+        this.reviewDate = reviewDate;
+    }
+    // Getters and Setters
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(Date reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "reviewId=" + reviewId +
+                ", userId=" + user.getUserId() +
+                ", reservationId=" + reservation.getReservationId() +
+                ", rating=" + rating +
+                ", comment='" + comment + '\'' +
+                ", reviewDate=" + reviewDate +
+                '}';
+    }
+}
