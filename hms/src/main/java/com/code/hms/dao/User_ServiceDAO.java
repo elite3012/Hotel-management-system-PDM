a1ce6fa5e38d5b0c663f47f3a28292063381e51b
Dinh Long <longdn120505@gmail.com>
2024-11-28 00:13:13 +0700

    feat: update UserServiceDAOImpl

--- hms/src/main/java/com/code/hms/dao/User_ServiceDAO.java
+++ hms/src/main/java/com/code/hms/dao/User_ServiceDAO.java
@@ -9,10 +9,10 @@ import com.code.hms.entities.User;
 public interface User_ServiceDAO {
     // find services by user id
     public List<Service> getServicesByUserID(int userId);
-
+    
     // find users by service id
     public List<User> getUsersByServiceID(int serviceId);
-
+    
     // save to database
     public void saveRoomReservation(Room_Reservation room_Reservation);
 }
