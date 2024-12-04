e8c5655825a87e9a746f8f4098ed46ed635d898b
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2024-12-04 15:32:03 +0700

    feat: add saveUser in UserDaoImpl

--- hms/src/main/java/com/code/hms/daoimpl/UserDaoImpl.java
+++ hms/src/main/java/com/code/hms/daoimpl/UserDaoImpl.java
@@ -2,7 +2,9 @@ package com.code.hms.daoimpl;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.MutationQuery;
 import org.hibernate.query.Query;
 
@@ -11,15 +13,19 @@ import com.code.hms.dao.UserDAO;
 import com.code.hms.entities.Reservation;
 import com.code.hms.entities.Review;
 import com.code.hms.entities.User;
+import com.code.hms.utils.LoggingEngine;
 
 
 public class UserDaoImpl implements UserDAO {
 
     private DataSourceFactory dsf = new DataSourceFactory();
     private Session session;
-    
+    private LoggingEngine logging;
+
     public UserDaoImpl() {
-        dsf.createConnection();
+        DataSourceFactory.createConnection();
+        logging = LoggingEngine.getInstance();
+
     }
 
     @Override
@@ -150,6 +156,24 @@ public class UserDaoImpl implements UserDAO {
         }
 
         return reviewList;
+    }
+    @Override
+    public void saveUser (User user) {
+        Transaction transaction = null;
+        try {
+            session = dsf.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            session.persist(user);
+
+            transaction.commit();
+            logging.setMessage("User  saved successfully.");
+        } catch (HibernateException e) {
+            if (transaction != null) transaction.rollback();
+            logging.setMessage("Error saving user: " + e.getLocalizedMessage());
+        } finally {
+            if (session != null) session.close();
+        }
     }
 
     @Override
